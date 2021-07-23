04741c7c1025042c7d7c3c48af478e8e13f00543
alexander.shidlovsky <Raccoonsasa1>
2021-07-23 16:17:25 +0300

    Update. New Version

--- Alisher_level_1/src/Variable.hpp
+++ Alisher_level_1/src/Variable.hpp
@@ -19,7 +19,7 @@ namespace storage {
 
     public:
         enum class Type {
-            STRUCT, DOUBLE, INT, BOOL
+            STRUCT, DOUBLE, INT, BOOL, STRING, INT_ARRAY, DOUBLE_ARRAY, BOOL_ARRAY, STRING_ARRAY
         };
 
         Variable(const std::string &name, Type type) {
@@ -32,11 +32,11 @@ namespace storage {
         }
 
         template<typename T>
-        Variable newMember(const std::string &name_var, T var) {
-            Variable temp = newVariable(name_var, var);
-            memory_.insert(std::make_pair(name_var, temp));
-            return temp;
+        Variable &newMember(const std::string &name_var, T var) {
+            memory_.insert(std::make_pair(name_var, newVariable(name_var, var)));
+            return memory_.at(name_var);
         }
+
 
         void list() {
             std::map<std::string, Variable>::iterator it;
@@ -44,10 +44,9 @@ namespace storage {
                 std::cout << it->first << ' ' << '\n';
         }
 
-        Variable newMember(Variable &variable_obj) {
-            Variable temp = newVariable(variable_obj);
-            memory_.insert(std::make_pair(variable_obj.name_, temp));
-            return temp;
+        Variable &newMember(Variable &variable_obj) {
+            memory_.insert(std::make_pair(variable_obj.name_, newVariable(variable_obj)));
+            return memory_.at(variable_obj.name_);
         }
 
         bool hasMember(const std::string &name_var) {
@@ -67,7 +66,7 @@ namespace storage {
         }
 
         std::vector<int> asIntArray() {
-            if (type_ == Type::INT) {
+            if (type_ == Type::INT_ARRAY) {
                 try {
                     return std::get<std::vector<int>>(value_);
                 }
@@ -87,8 +86,16 @@ namespace storage {
             }
         }
 
+        std::string asString() {
+            if (type_ == Type::STRING) {
+                return std::get<std::string>(value_);
+            } else {
+                throw "Error get type";
+            }
+        }
+
         std::vector<double> asDoubleArray() {
-            if (type_ == Type::DOUBLE) {
+            if (type_ == Type::DOUBLE_ARRAY) {
                 try {
                     return std::get<std::vector<double>>(value_);
                 }
@@ -109,7 +116,7 @@ namespace storage {
         }
 
         std::vector<bool> asBoolArray() {
-            if (type_ == Type::BOOL) {
+            if (type_ == Type::BOOL_ARRAY) {
                 try {
                     return std::get<std::vector<bool>>(value_);
                 }
@@ -138,15 +145,58 @@ namespace storage {
             }
         }
 
+        void operator=(
+                const std::variant<int, double, bool, std::string, std::vector<int>, std::vector<double>, std::vector<bool>> &var) {
+            switch (this->type_) {
+                case Variable::Type::BOOL :
+                    try {
+                        if (std::get_if<std::string>(&var) == nullptr) throw  "Types don't match";
+                        std::get<bool>(var);
+                    } catch (int) {
+                        std::cerr << "Types don't match" << std::endl;
+                    }
+                    break;
+                case Variable::Type::DOUBLE:
+                    try {
+                        std::get<double>(var);
+                    } catch (int) {
+                        std::cerr << "Types don't match" << std::endl;
+                    }
+                    break;
+                case Variable::Type::INT:
+                    try {
+                        std::get<int>(var);
+                    } catch (int) {
+                        std::cerr << "Types don't match" << std::endl;
+                    }
+                    break;
+                case Variable::Type::STRING:
+                    try {
+                        std::get<std::string>(var);
+                    } catch (int) {
+                        std::cerr << "Types don't match" << std::endl;
+                    }
+                    break;
+                default:
+                    throw "Types don't match";
+            }
+        }
+
     private:
 
         Type type_;
         std::string name_;
-        std::variant<int, double, bool, std::vector<int>, std::vector<double>, std::vector<bool>> value_;
+        std::variant<int, double, bool, std::string, std::vector<int>, std::vector<double>, std::vector<bool>> value_;
         std::map<std::string, Variable> memory_;
 
         Variable newVariable(const std::string &name_var, int var) {
             Variable temp = Variable(name_var, Type::INT);
+            temp.value_ = var;
+            return temp;
+        }
+
+        Variable newVariable(const std::string &name_var, std::vector<int> var) {
+            Variable temp = Variable(name_var, Type::INT_ARRAY);
             temp.value_ = var;
             return temp;
         }
@@ -163,20 +213,14 @@ namespace storage {
             return temp;
         }
 
-        Variable newVariable(const std::string &name_var, std::vector<int> var) {
-            Variable temp = Variable(name_var, Type::INT);
-            temp.value_ = var;
-            return temp;
-        }
-
         Variable newVariable(const std::string &name_var, std::vector<double> var) {
-            Variable temp = Variable(name_var, Type::DOUBLE);
+            Variable temp = Variable(name_var, Type::DOUBLE_ARRAY);
             temp.value_ = var;
             return temp;
         }
 
         Variable newVariable(const std::string &name_var, std::vector<bool> var) {
-            Variable temp = Variable(name_var, Type::BOOL);
+            Variable temp = Variable(name_var, Type::BOOL_ARRAY);
             temp.value_ = var;
             return temp;
         }
@@ -190,16 +234,17 @@ namespace storage {
         }
 
         friend bool operator==(const Variable &var1,
-                               const std::variant<int, double, bool, std::vector<int>, std::vector<double>, std::vector<bool>> &var2);
+                               const std::variant<int, double, bool, std::string, std::vector<int>, std::vector<double>, std::vector<bool>> &var2);
 
         friend bool operator!=(const Variable &var1,
-                               const std::variant<int, double, bool, std::vector<int>, std::vector<double>, std::vector<bool>> &var2);
+                               const std::variant<int, double, bool, std::string, std::vector<int>, std::vector<double>, std::vector<bool>> &var2);
+
 
     };
 
 
     bool operator==(const Variable &var1,
-                    const std::variant<int, double, bool, std::vector<int>, std::vector<double>, std::vector<bool>> &var2) {
+                    const std::variant<int, double, bool, std::string, std::vector<int>, std::vector<double>, std::vector<bool>> &var2) {
         try {
             if (var1.value_ == var2) {
                 return true;
@@ -213,7 +258,7 @@ namespace storage {
     }
 
     bool operator!=(const Variable &var1,
-                    const std::variant<int, double, bool, std::vector<int>, std::vector<double>, std::vector<bool>> &var2) {
+                    const std::variant<int, double, bool, std::string, std::vector<int>, std::vector<double>, std::vector<bool>> &var2) {
         try {
             if (var1.value_ != var2) {
                 return true;
